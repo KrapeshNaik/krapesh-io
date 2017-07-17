fb18b51f4f4113352023a0779bbfc770910ba628
Krapesh <krapesh.naik@redbus.com>
2017-07-17 13:35:36 +0530

    remove distribution folder

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 (function () {
     var config = {
-        apiKey: "AIzaSyBK4rmrHzJU4473G8EEocgkRntes6WdohQ",
-        authDomain: "https://krapesh-723c7.firebaseapp.com",
-        databaseURL: "https://krapesh-723c7.firebaseio.com/",
-        storageBucket: "gs://krapesh-723c7.appspot.com/",
-        messagingSenderId: "114891145688",
+        apiKey: 'AIzaSyBK4rmrHzJU4473G8EEocgkRntes6WdohQ',
+        authDomain: 'https://krapesh-723c7.firebaseapp.com',
+        databaseURL: 'https://krapesh-723c7.firebaseio.com/',
+        storageBucket: 'gs://krapesh-723c7.appspot.com/',
+        messagingSenderId: '114891145688',
     };
     firebase.initializeApp(config);
 
