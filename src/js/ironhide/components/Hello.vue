2fa770832c388473785aa00efd2212b85f3e0b95
Krapesh <krapeshnaik@gmail.com>
2017-07-19 09:50:13 +0530

    grid view

--- src/js/ironhide/components/Hello.vue
+++ src/js/ironhide/components/Hello.vue
@@ -2,20 +2,22 @@
     <div class="hello">
         <div class="card">
             <div class="pure-g">
-                <div class="pure-u-1-1">
-                    <img class="pure-img avatar" src="https://vuejs.org/images/logo.png">
+                <div class="avatar pure-u-1 pure-u-md-1-2">
+                    <img class="pure-img" src="https://vuejs.org/images/logo.png">
                 </div>
-                <div class="pure-u-1-1">
-                    <p>Full</p>
-                </div>
-                <div class="pure-u-1-3">
-                    <p>Thirds</p>
-                </div>
-                <div class="pure-u-1-3">
-                    <p>Thirds</p>
-                </div>
-                <div class="pure-u-1-3">
-                    <p>Thirds</p>
+                <div class="summary pure-u-1 pure-u-md-1-2">
+                    <div class="pure-u-1-1">
+                        <p>Full</p>
+                    </div>
+                    <div class="pure-u-1-3">
+                        <p>Thirds</p>
+                    </div>
+                    <div class="pure-u-1-3">
+                        <p>Thirds</p>
+                    </div>
+                    <div class="pure-u-1-3">
+                        <p>Thirds</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,6 +37,13 @@ export default {
 
 <style scoped>
 .avatar {
+    padding: 1em;
+}
+
+.avatar img {
     margin: 0 auto;
+    border-radius: 50%;
+    background-color: aqua;
+    width: 45%;
 }
 </style>
